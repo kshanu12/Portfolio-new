d20d37c5d9f905db6ca7910ff89b70eb22c0027e
kshanu12 <kumarshanu38370@gmail.com>
2025-04-22 22:37:38 +0530

    initial commit

---
+++ portfolio/src/pages/index.js
@@ -0,0 +1,11 @@
+import Header from "@/components/header";
+
+export default function Home() {
+  return (
+    <>
+      <img src="gradient" alt="gradient" />
+      <div className="layer-blur" />
+      <Header />
+    </>
+  );
+}
