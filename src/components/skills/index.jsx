419d5153b082284a812dc069670c575932cc8a29
kshanu12 <kumarshanu38370@gmail.com>
2025-04-28 00:53:25 +0530

    added icon cloud for skills

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -1,17 +1,21 @@
 import styles from './style.module.css';
 import { skills } from '@/constants/skills';
 import TreeNode from './TreeNode';
+import DynamicIconCloud from './skillIconCloud';
 
 const SkillSection = () => {
     return (
         <div className={styles.skillSection}>
             <p className="subTitle">WHAT I BRING TO THE TABLE</p>
             <h1 className="title">Skills.</h1>
-            <ul className={styles.tree}>
-                {skills.map(node => (
-                    <TreeNode key={node.id} node={node} />
-                ))}
-            </ul>
+            <div style={{ display: "flex" }}>
+                <ul className={styles.tree}>
+                    {skills.map(node => (
+                        <TreeNode key={node.id} node={node} />
+                    ))}
+                </ul>
+                <DynamicIconCloud />
+            </div>
         </div>
     );
 };
---
+++ src/components/skills/skillIconCloud/index.jsx
@@ -0,0 +1,83 @@
+import React,{useState} from 'react';
+import { Cloud, renderSimpleIcon, fetchSimpleIcons } from 'react-icon-cloud';
+
+const useIcons = (slugs) => {
+    const [icons, setIcons] = useState()
+    React.useEffect(() => { fetchSimpleIcons({ slugs }).then(setIcons) }, [])
+
+    if (icons) {
+        return Object.values(icons.simpleIcons).map((icon) => renderSimpleIcon({
+            icon,
+            size: 42,
+            aProps: {
+                onClick: (e) => e.preventDefault()
+            }
+        }))
+    }
+
+    return <a>Loading</a>
+}
+
+const slugs = [
+    'c',
+    'cplusplus',
+    'javascript',
+    'typescript',
+    'python',
+    'graphql',
+    'react',
+    'nextdotjs',
+    'redux',
+    'zustand',
+    'tailwindcss',
+    'mui',
+    'html5',
+    'css3',
+    'vitest',
+    'nodedotjs',
+    'nestjs',
+    'express',
+    'postgresql',
+    'mongodb',
+    'git',
+    'swagger',
+    'docker',
+    'jira',
+    'confluence',
+    'linux',
+    'postman'
+];
+
+
+function DynamicIconCloud() {
+    const icons = useIcons(slugs);
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: 'calc(100vh - 25rem)',
+                position: 'relative',
+            }}
+        >
+                <Cloud
+                    options={{
+                        clickToFront: 500,
+                        depth: 1,
+                        initial: [0.03, -0.03],
+                        wheelZoom: false,
+                        dragControl: true,
+                        decel: 0.95,
+                        outlineColour: 'transparent',
+                        reverse: true,
+                        speed: 0.5,
+                    }}
+                >
+                    {icons}
+                </Cloud>
+        </div>
+    );
+}
+
+export default DynamicIconCloud;
